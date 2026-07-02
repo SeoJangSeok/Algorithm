5c7afe625f4e38e6664454f4d6e40d3156174cf4
S__J___S___ <129780104+SeoJangSeok@users.noreply.github.com>
2026-07-03 00:49:50 +0900

    Refine ID recommendation steps for efficiency

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -10,16 +10,13 @@ def solution(new_id):
     while '..' in answer:
         answer = answer.replace('..', '.')
     # 4단계
-    answer = answer[1:] if answer[0] == '.' and len(answer) > 1 else answer
-    answer = answer[:-1] if answer[-1] == '.' else answer
+    answer = answer.strip('.')
     # 5단계
     answer = 'a' if answer == '' else answer
     # 6단계
-    if len(answer) >= 16:
-        answer = answer[:15]
-        if answer[-1] =='.':
-            answer = answer[:-1]
+    answer = answer[:15].rstrip('.')
     # 7단계
-    if len(answer) <= 3:
-        answer = answer + answer[-1] * (3 - len(answer))
+    while len(answer) < 3:
+        answer += answer[-1]
+        
     return answer
