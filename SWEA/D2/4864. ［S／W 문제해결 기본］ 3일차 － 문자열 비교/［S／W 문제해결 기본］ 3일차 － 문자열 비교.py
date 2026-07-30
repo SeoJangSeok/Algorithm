3fcb4a1f01a386a7dfcb6321767d70c87e698acb
S__J___S___ <129780104+SeoJangSeok@users.noreply.github.com>
2026-07-30 22:39:47 +0900

    [D2] Title: [S/W 문제해결 기본] 3일차 - 문자열 비교, Time: 53 ms, Memory: 50,560 KB -BaekjoonHub

--- SWEA/D2/4864. ［S／W 문제해결 기본］ 3일차 － 문자열 비교/［S／W 문제해결 기본］ 3일차 － 문자열 비교.py
+++ SWEA/D2/4864. ［S／W 문제해결 기본］ 3일차 － 문자열 비교/［S／W 문제해결 기본］ 3일차 － 문자열 비교.py
@@ -1,10 +1,52 @@
+# KMP 알고리즘 사용
+def make_lps(pattern):
+    lps = [0] * len(pattern)
+    # i: 현재 LPS 값을 구할 위치
+    # j: 현재까지 일치한 접두사의 길이, i와 비교할 접두사의 인덱스
+    i, j = 1, 0
+    
+    while i < len(pattern):
+        if pattern[i] == pattern[j]:
+            j += 1
+            lps[i] = j
+            i += 1
+        else:
+            if j > 0:
+                j = lps[j - 1]
+            else:
+                lps[i] = 0
+                i += 1
+    return lps
+
+def kmp_search(text, pattern):
+        lps = make_lps(pattern)
+        
+        # i: text index
+        # j: pattern index
+        i, j = 0, 0
+        
+        while i < len(text):
+        # 두 문자가 같은 경우
+            if text[i] == pattern[j]:
+                i += 1
+                j += 1
+                # Pattern의 마지막까지 일치한 경우
+                if j == len(pattern):
+                    return 1
+            # 두 문자가 다른 경우
+            else:
+                if j > 0:
+                    j = lps[j - 1]
+                else:
+                    i += 1
+        return 0
+
 T = int(input())
 
 for test_case in range(1, T+1):
-    str1 = input()
-    str2 = input()
+    pattern = input()
+    text = input()
     
-    if str1 in str2:
-        print(f'#{test_case} 1')
-    else:
-        print(f'#{test_case} 0')
+    result = kmp_search(text, pattern)
+    
+    print(f'#{test_case} {result}')
