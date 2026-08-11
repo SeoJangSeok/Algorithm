2951d76ab5ff4730d1da7377e06417bd98add626
S__J___S___ <129780104+SeoJangSeok@users.noreply.github.com>
2026-08-11 16:34:18 +0900

    [D2] Title: [S/W 문제해결 기본] 5일차 - 미로, Time: 93 ms, Memory: 62,848 KB -BaekjoonHub

--- SWEA/D2/4875. ［S／W 문제해결 기본］ 5일차 － 미로/［S／W 문제해결 기본］ 5일차 － 미로.py
+++ SWEA/D2/4875. ［S／W 문제해결 기본］ 5일차 － 미로/［S／W 문제해결 기본］ 5일차 － 미로.py
@@ -1,18 +1,22 @@
-# 백트래킹 사용 X
+# 백트래킹 사용
 def dfs(row, col):
-    if maze[row][col] == 3: # 도착점에 도달한 경우
+    if maze[row][col] == 3: # 도착점에 도달하면
         return 1
     
-    visited[row][col] = 1 # 현재 위치 방문 처리
+    # 현재 위치 방문 처리
+    visited[row][col] = 1
     
-    # 현재 위치에서 상하좌우로 이동할 수 있는지 확인
     for dr, dc in moves:
         next_row, next_col = row + dr, col + dc
         
-        if 0 <= next_row < N and 0 <= next_col < N: # 미로 범위 내라면
-            if maze[next_row][next_col] != 1 and visited[next_row][next_col] == 0: # 벽이 아니고 방문하지 않은 경우
-                if dfs(next_row, next_col):
+        if 0 <= next_row < N and 0 <= next_col < N: # 미로 범위 내에 있는지 확인
+            if maze[next_row][next_col] != 1 and visited[next_row][next_col] == 0: # 벽이 아니고 방문하지 않은 곳이면
+                if dfs(next_row, next_col): # 다음 위치로 이동
                     return 1
+    
+    # 현재 위치에서 도착점을 찾지 못하면 방문 처리 취소
+    visited[row][col] = 0
+    
     return 0
 
 T = int(input())
@@ -20,10 +24,13 @@ T = int(input())
 for test_case in range(1, T + 1):
     # N: 미로의 크기
     N = int(input())
+    
     maze = []
-    visited = [[0] * N for _ in range(N)] # 방문 여부를 저장할 리스트
-    start = () # 출발점 좌표
-    moves = [(-1, 0), (1, 0), (0, -1), (0, 1)] # 상하좌우 이동 좌표
+    visited = [[0] * N for _ in range(N)]
+    
+    start = (None, None) # 출발점 좌표
+    
+    moves = ((-1, 0), (1, 0), (0, -1), (0, 1)) # 상하좌우 이동 좌표
     
     for i in range(N):
         # 미로의 통로와 벽에 대한 정보
