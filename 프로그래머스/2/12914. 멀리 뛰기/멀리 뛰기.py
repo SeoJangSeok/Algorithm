404648344191127df8a804af8bae2a66ea25a6bd
S__J___S___ <129780104+SeoJangSeok@users.noreply.github.com>
2026-08-24 23:10:10 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.15 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,12 @@
+def solution(n):
+    if n < 3:
+        return n
+    
+    jump = [0] * (n + 1)
+    jump[1] = 1
+    jump[2] = 2
+    
+    for i in range(3, n + 1):
+        jump[i] = (jump[i - 1] + jump[i - 2]) % 1234567
+    
+    return jump[n]
